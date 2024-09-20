d392c0af3667934a0fae33ce0372f1ca66e943ae
Wesley <wesmith314@gmail.com>
2024-09-19 22:25:28 -0700

    adds button ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import { Bento } from "@/components/Bento";
 import { CardCarousel } from "@/components/CardCarousel";
 import Footer from "@/components/Footer";
 import { Grid } from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
+import { ButtonsCard } from "@/components/ui/tailwindcss-button";
 import Image from "next/image";
 import 'semantic-ui-css/semantic.min.css'
 
@@ -16,6 +18,7 @@ export default function Home() {
        <CardCarousel />
        <Grid />
        <Bento />
+       <ButtonsCard />
        <Footer />
       </div>
     </main>
